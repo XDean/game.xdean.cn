a18972cf6d8ef74daa8fdc27bc202037da64566d
dxu <dean.xu@asml.com>
2022-01-27 17:32:10 +0800

    next-auth add email

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { TypeORMLegacyAdapter } from '@next-auth/typeorm-legacy-adapter';
 import NextAuth from 'next-auth';
+import EmailProvider from 'next-auth/providers/email';
 import GithubProvider from 'next-auth/providers/github';
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
 
 export default NextAuth({
   providers: [
@@ -7,5 +10,34 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: Number(process.env.EMAIL_SERVER_PORT),
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+        secure: true,
+      },
+      from: process.env.EMAIL_FROM,
+      secret: process.env.SECRET,
+    }),
   ],
+  adapter: TypeORMLegacyAdapter({
+    type: 'mysql',
+    host: process.env.MYSQL_HOST,
+    port: Number(process.env.MYSQL_PORT),
+    database: process.env.MYSQL_DATABASE,
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    synchronize: true,
+  } as MysqlConnectionOptions),
+  secret: process.env.SECRET,
+  session: {
+    strategy: 'jwt',
+  },
+  jwt: {
+    secret: process.env.SECRET,
+  },
 });
