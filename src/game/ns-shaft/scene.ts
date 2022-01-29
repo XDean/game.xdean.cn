a528ab01a551d30d98cad03b059812b3382f1050
dxu <dean.xu@asml.com>
2022-01-29 14:40:23 +0800

    [ns-shaft] player control

--- src/game/ns-shaft/scene.ts
+++ src/game/ns-shaft/scene.ts
@@ -4,6 +4,7 @@ import { loadAsset } from '../../phaser/util';
 import { Assets } from './assets';
 
 export class NSShaftScene extends Phaser.Scene {
+
   constructor() {
     super('ns-shaft');
   }
@@ -14,10 +15,6 @@ export class NSShaftScene extends Phaser.Scene {
   }
 
   create() {
-    const platforms = this.physics.add.staticGroup();
-    (platforms.create(0, 400, Assets.wall.key) as Phaser.Physics.Arcade.Image).setScale(2).refreshBody();
-    (platforms.create(600, 400, Assets.wall.key) as Phaser.Physics.Arcade.Image).setScale(2).refreshBody();
-    const player = this.add.sprite(200, 200, Assets.player.key);
     Object.values(Assets)
       .filter(e => e.type === 'sprite-sheet')
       .map(e => e as SpriteSheet<any>)
@@ -29,7 +26,30 @@ export class NSShaftScene extends Phaser.Scene {
           });
         });
       });
-    player.play(Assets.player.anims.stay.key)
 
+    const platforms = this.physics.add.staticGroup();
+    (platforms.create(0, 400, Assets.wall.key) as Phaser.Physics.Arcade.Image).setScale(2).refreshBody();
+    (platforms.create(600, 400, Assets.wall.key) as Phaser.Physics.Arcade.Image).setScale(2).refreshBody();
+    (platforms.create(300, 12, Assets.ceiling.key) as Phaser.Physics.Arcade.Image).setScale(1.5).refreshBody();
+
+    const player = this.physics.add.sprite(200, 200, Assets.player.key);
+    player.setBounce(0.1);
+    player.setCollideWorldBounds(true);
+    player.play(Assets.player.anims.stay.key);
+    this.physics.add.collider(player, platforms);
+    const cursors = this.input.keyboard.createCursorKeys();
+
+    this.update = () => {
+      if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+        player.anims.play(Assets.player.anims.left.key, true);
+      } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        player.anims.play(Assets.player.anims.right.key, true);
+      } else {
+        player.setVelocityX(0);
+        player.anims.play(Assets.player.anims.stay.key);
+      }
+    };
   }
 }
